a6937b41384b16c4d546831f025302440b9e7381
Isaiah Peng <isaiah.peng@vcint.com>
2012-11-24 23:59:49 +0100

    precompile active_admin.css

--- db/schema.rb
+++ db/schema.rb
@@ -62,12 +62,6 @@ ActiveRecord::Schema.define(:version => 20121124171150) do
   add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
-  create_table "conversations", :force => true do |t|
-    t.string   "subject",    :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-  end
-
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.text     "description"
@@ -84,24 +78,6 @@ ActiveRecord::Schema.define(:version => 20121124171150) do
     t.integer "user_id"
     t.integer "group_id"
   end
-
-  create_table "notifications", :force => true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              :default => ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                :default => false
-    t.datetime "updated_at",                              :null => false
-    t.datetime "created_at",                              :null => false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-  end
-
-  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -134,20 +110,6 @@ ActiveRecord::Schema.define(:version => 20121124171150) do
     t.integer "user_id"
   end
 
-  create_table "receipts", :force => true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                                  :null => false
-    t.boolean  "is_read",                       :default => false
-    t.boolean  "trashed",                       :default => false
-    t.boolean  "deleted",                       :default => false
-    t.string   "mailbox_type",    :limit => 25
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-  end
-
-  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -161,7 +123,7 @@ ActiveRecord::Schema.define(:version => 20121124171150) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name",                   :default => ""
+    t.string   "name"
     t.string   "workflow_state"
     t.text     "biography"
     t.integer  "mentor_id"
@@ -170,9 +132,5 @@ ActiveRecord::Schema.define(:version => 20121124171150) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  add_foreign_key "notifications", "conversations", :name => "notifications_on_conversation_id"
-
-  add_foreign_key "receipts", "notifications", :name => "receipts_on_notification_id"
 
 end
