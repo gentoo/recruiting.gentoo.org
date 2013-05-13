f836530ae09834466f984895a298fabc7ce93945
Isaiah Peng <issaria@gmail.com>
2013-05-13 21:52:57 +0200

    updat db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(:version => 20130513125351) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
