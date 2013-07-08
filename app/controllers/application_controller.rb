e06d7f3181b712b2388a94b72c5b44cea5146898
isaiah <issaria@gmail.com>
2013-07-08 22:29:20 +0200

    remove trial period notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter do
-    flash[:alert] = "This application is under trial period and all data will be cleared on final release."
-  end
-
   def authenticate_admin_user!
     redirect_to root_path, alert: "WARNING: You are not authorized to this area." unless current_user.try(:admin?)
   end
