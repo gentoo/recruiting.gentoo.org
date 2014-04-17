82211d2bd0518940542a1752d71632cf0e143867
isaiah <issaria@gmail.com>
2014-04-17 22:05:24 +0200

    fix cucumber specs

--- features/step_definitions/quiz_subscription_steps.rb
+++ features/step_definitions/quiz_subscription_steps.rb
@@ -1,18 +1,18 @@
-Given /^sample question categories exist$/ do
+Given(/^sample question categories exist$/) do
   ["Developer Quiz", "Staffer Quiz"].inject(1) do |question_count, group_name|
     group = FactoryGirl.create :group, name: group_name
-    FactoryGirl.create :question, group: group, title: "Organizational structure questions #{question_count}"
-    FactoryGirl.create :question, group: group, title: "Organizational structure questions #{question_count + 1}"
+    FactoryGirl.create :question, group: group, content: "Organizational structure questions #{question_count}"
+    FactoryGirl.create :question, group: group, content: "Organizational structure questions #{question_count + 1}"
     question_count + 2
   end
 end
 
-When /^I subscribe to question group "([^"]*)"$/ do |group|
+When(/^I subscribe to question group "([^"]*)"$/) do |group|
   subscribe_link = page.find :xpath, "//td[text()='#{group}']/..//a[contains(text(), 'Subscribe')]"
   subscribe_link.click
 end
 
-Then /^I should be subscribed to all question groups$/ do
+Then(/^I should be subscribed to all question groups$/) do
   User.count.should == 1
   User.last.groups.count.should == Group.count
 end
